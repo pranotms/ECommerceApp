78c86ff7effe060a0a052729578e3c95e3a527d8
Shinde, Pranoti <Pranoti.Shinde@bridgenext.com>
2024-05-09 12:44:38 +0530

    saved changes

--- ECommerce.API/ECommerce.Service/obj/Debug/net6.0/ECommerce.Services.AssemblyInfo.cs
+++ ECommerce.API/ECommerce.Service/obj/Debug/net6.0/ECommerce.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e45c3c8a17093793a1249479dac6b043d1f059")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ECommerce.API/ECommerceApp/obj/Debug/net6.0/ECommerceAPI.AssemblyInfo.cs
+++ ECommerce.API/ECommerceApp/obj/Debug/net6.0/ECommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e45c3c8a17093793a1249479dac6b043d1f059")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
